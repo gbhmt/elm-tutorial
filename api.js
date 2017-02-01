4e281474c92ec91a61dfac828b701ffe306def11
Taylor Herron <taylorbherron@gmail.com>
2017-02-01 14:27:32 -0500

    Initial commit, after implementing initial routing

---
+++ api.js
@@ -0,0 +1,10 @@
+import jsonServer from 'json-server';
+
+const server = jsonServer.create();
+
+server.use(jsonServer.defaults());
+
+const router = jsonServer.router('db.json');
+server.use(router);
+
+server.listen(4000);
